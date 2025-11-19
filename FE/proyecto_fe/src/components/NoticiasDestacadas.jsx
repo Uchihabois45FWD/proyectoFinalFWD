a18c6f0119b0d62942a2a58f372f56282d8006eb
Uchihabois45FWD <fwdadriandcc0607@gmail.com>
2025-11-19 09:18:16 -0600

    NoticiasDestacadas corregidas

--- FE/proyecto_fe/src/components/NoticiasDestacadas.jsx
+++ FE/proyecto_fe/src/components/NoticiasDestacadas.jsx
@@ -1,17 +1,13 @@
-import "../styles/CursosDestacados.css"
+import "../styles/NoticiasDestacadas.css"
 
-export default function NoticiasDestacadas({titulo="Noticia", descripcion="Descripción", primer_dia="2023-01-01", ultimo_dia="", cupos="0", inscructor=""}) {
+export default function NoticiasDestacadas({titulo="Noticia", descripcion="Descripción",dia_publicacion=""}) {
     return (
         <div className="curso-card">
             <div className="curso-icon"></div>
             <h3>{titulo}</h3>
             <p>{descripcion}</p>
-            <div className="cont-datos">
-                <p className="dias-curso">{primer_dia} {ultimo_dia}</p>
-                <br />
-                <p className="dias-curso">{cupos} cupos</p>
-            </div>
-            <p className="dias-curso">{inscructor}</p>
+            <p>{dia_publicacion}</p>
+           
         </div>
     );
 }
--- FE/proyecto_fe/src/pages/PaginaInicio.jsx
+++ FE/proyecto_fe/src/pages/PaginaInicio.jsx
@@ -2,8 +2,8 @@ import "../styles/Hero.css"
 import Hero from '../components/Hero'
 import { useEffect, useState } from "react"
 import { getData } from "../services/fetch"
-import ListaCursos from "../components/ListaCursos"
-import ListaNoticiasDestacadas from "../components/ListaNoticiasDestacadas"
+import CursosDestacados from "../components/CursosDestacados"
+import NoticiasDestacadas from "../components/NoticiasDestacadas"
 
 function PaginaInicio() {
     const [cursos, setCursos] = useState([])
@@ -32,12 +32,30 @@ function PaginaInicio() {
 
             <h3 className='titulo-cursos'>Cursos destacados</h3>
             <div className="cont-cursos">
-                <ListaCursos cursos={cursos} />
+                {cursos.map((curso) => (
+                    <CursosDestacados
+                        key={curso.id}
+                        titulo={curso.nombre_curso}
+                        descripcion={curso.descripcion_curso}
+                        primer_dia={curso.primer_dia}
+                        ultimo_dia={curso.ultimo_dia}
+                        cupos={curso.limite_cupos}
+                        inscructor={curso.nombre_instructor + ' ' + curso.apellido_instructor}
+                    />
+                ))}
             </div>
 
             <h3 className='titulo-noticias'>Noticias destacadas</h3>
             <div className="cont-noticias">
-                <ListaNoticiasDestacadas noticias={noticias} />
+                {noticias.filter(noticia => noticia.destacado).map((noticia) => (
+                    <NoticiasDestacadas
+                        key={noticia.id}
+                        titulo={noticia.titulo_noticia}
+                        descripcion={noticia.descripcion_noticia}
+                        primer_dia={noticia.dia_de_notificacion}
+                        dia_publicacion={noticia.dia_de_notificacion}
+                    />
+                ))}
             </div>
         </>
     )
