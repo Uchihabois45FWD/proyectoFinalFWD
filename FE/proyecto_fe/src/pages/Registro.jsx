387bde89279673bb8a750d1eb600ee0aec9fb310
Uchihabois45FWD <fwdadriandcc0607@gmail.com>
2025-12-12 00:14:25 -0600

    respaldo

--- FE/proyecto_fe/src/pages/Registro.jsx
+++ FE/proyecto_fe/src/pages/Registro.jsx
@@ -1,6 +1,7 @@
 import Footer from '../components/Global/Footer'
 import Navbar from '../components/Global/Navbar'
 import RegistroComp from '../components/Login y Register/RegistroComp'
+import "../styles/RegisterPage.css"
 
 function Registro() {
   return (
